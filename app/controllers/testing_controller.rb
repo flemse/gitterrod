02cc7e781677d089a518ad24e4eff2eff1d02d73
Flemming Thesbjerg <flemming.thesbjerg@gmail.com>
2015-10-20 20:56:19 +0200

    add changes

--- app/controllers/testing_controller.rb
+++ app/controllers/testing_controller.rb
@@ -1,2 +1,10 @@
 class TestingController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: :push
+  def index
+    @client = Octokit::Client.new
+  end
+
+  def push
+    binding.pry
+  end
 end
