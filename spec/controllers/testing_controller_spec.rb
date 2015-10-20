3ed4d64cc3fdc1edf4f9c277614a0b9844b601fd
Flemming Thesbjerg <flemming.thesbjerg@gmail.com>
2015-10-20 23:25:42 +0200

    add guard and rspec files

--- spec/controllers/testing_controller_spec.rb
+++ spec/controllers/testing_controller_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe TestingController, type: :controller do
+  let(:client) {
+    Octokit::Client.new(
+      login: ENV['OCTOKIT_TEST_GITHUB_LOGIN'],
+      password: ENV['OCTOKIT_TEST_GITHUB_PASSWORD']
+    )
+  }
+  describe 'Getting pushing a branch to GH' do
+    it 'has access' do
+      client.create_contents(
+        'gitterrod/test_repo',
+        "testfile#{DateTime.now.to_s}.txt",
+        'text commit #1',
+        'file content',
+        branch: 'master'
+      )
+    end
+  end
 
 end
